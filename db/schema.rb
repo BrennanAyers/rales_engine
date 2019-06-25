3afff26667a3433ccd0ad5d93f505379507e3985
Brennan Ayers <brennan.f.ayers@gmail.com>
2019-06-25 14:18:45 -0600

    Add Merchant migration, update Schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20190625200945) do
+ActiveRecord::Schema.define(version: 20190625201734) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -20,6 +20,12 @@ ActiveRecord::Schema.define(version: 20190625200945) do
     t.text "description"
     t.integer "unit_price"
     t.integer "merchant_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "merchants", force: :cascade do |t|
+    t.string "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
