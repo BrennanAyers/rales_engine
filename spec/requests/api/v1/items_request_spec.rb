0f2d09bbbef73163e035c7ee499b88c5e04d9c36
Brennan Ayers <brennan.f.ayers@gmail.com>
2019-06-26 16:39:09 -0600

    Add Items Request #show spec, Add ItemsController #show method

--- spec/requests/api/v1/items_request_spec.rb
+++ spec/requests/api/v1/items_request_spec.rb
@@ -12,4 +12,19 @@ describe "Items API" do
 
     expect(items.count).to eq(3)
   end
+
+  it "can get one item by its id" do
+    object = create(:item)
+
+    get "/api/v1/items/#{object.id}"
+
+    item = JSON.parse(response.body)["data"]
+
+    expect(response).to be_successful
+    expect(item["type"]).to eq("item")
+    expect(item["attributes"]["id"]).to eq(object.id)
+    expect(item["attributes"]["description"]).to eq(object.description)
+    expect(item["attributes"]["unit_price"]).to eq(object.unit_price)
+    expect(item["attributes"]["merchant_id"]).to eq(object.merchant_id)
+  end
 end
