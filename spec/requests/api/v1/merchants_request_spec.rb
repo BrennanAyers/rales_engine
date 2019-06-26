e2af6793702923b1d167102424b4749a9a40d37d
Brennan Ayers <brennan.f.ayers@gmail.com>
2019-06-26 16:20:50 -0600

    Add #show spec to Merchant Request spec

--- spec/requests/api/v1/merchants_request_spec.rb
+++ spec/requests/api/v1/merchants_request_spec.rb
@@ -12,4 +12,17 @@ describe "Merchants API" do
 
     expect(merchants.count).to eq(3)
   end
+
+  it "can get one merchant by its id" do
+    object = create(:merchant)
+
+    get "/api/v1/merchants/#{object.id}"
+
+    merchant = JSON.parse(response.body)["data"]
+
+    expect(response).to be_successful
+    expect(merchant["type"]).to eq("merchant")
+    expect(merchant["attributes"]["id"]).to eq(object.id)
+    expect(merchant["attributes"]["name"]).to eq(object.name)
+  end
 end
