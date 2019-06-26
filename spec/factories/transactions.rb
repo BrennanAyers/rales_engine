318cb12bc9ed382b26c5a4a8a0cefedc157b27b9
Brennan Ayers <brennan.f.ayers@gmail.com>
2019-06-26 17:05:01 -0600

    Update Transaction migration/schema to store CC# as string, Update Transnsaction Factory to use CC# string

--- spec/factories/transactions.rb
+++ spec/factories/transactions.rb
@@ -1,7 +1,7 @@
 FactoryBot.define do
   factory :transaction do
     invoice
-    credit_card_number { 1234567890123456 }
+    credit_card_number { "1234567890123456" }
     credit_card_expiration_date { "12/34" }
     result { "success" }
   end
