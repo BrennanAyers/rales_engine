9ab0e9fc1bcea04ed9469b54a97b2e3971dbed85
Brennan Ayers <brennan.f.ayers@gmail.com>
2019-06-27 14:45:09 -0600

    Add Model specs for Merchant covering Class Methods

--- spec/models/merchant_spec.rb
+++ spec/models/merchant_spec.rb
@@ -5,4 +5,36 @@ RSpec.describe Merchant, type: :model do
     it { should have_many :items }
     it { should have_many :invoices }
   end
+
+  describe 'class methods' do
+    before :each do
+      @merchant_1 = create(:merchant)
+      @merchant_2 = create(:merchant)
+      @merchant_3 = create(:merchant)
+      @customer_1 = create(:customer)
+      @customer_2 = create(:customer)
+      @item_1 = create(:item, unit_price: 100, merchant: @merchant_1)
+      @item_2 = create(:item, unit_price: 1000, merchant: @merchant_2)
+      @item_3 = create(:item, unit_price: 200, merchant: @merchant_3)
+      @invoice_1 = create(:invoice, merchant: @merchant_1, customer: @customer_1)
+      @invoice_item_1 = create(:invoice_item, quantity: 30, unit_price: 100, invoice: @invoice_1, item: @item_1)
+      @transaction_1 = create(:transaction, invoice: @invoice_1)
+      @invoice_2 = create(:invoice, merchant: @merchant_2, customer: @customer_2)
+      @invoice_item_2 = create(:invoice_item, quantity: 20, unit_price: 1000, invoice: @invoice_2, item: @item_2)
+      @transaction_2 = create(:transaction, invoice: @invoice_2)
+      @invoice_3 = create(:invoice, merchant: @merchant_3, customer: @customer_2)
+      @invoice_item_3 = create(:invoice_item, quantity: 21, unit_price: 200, invoice: @invoice_3, item: @item_3)
+      @transaction_3 = create(:transaction, invoice: @invoice_3)
+    end
+
+    it 'most_revenue' do
+      expect(Merchant.most_revenue(1)).to eq([@merchant_2])
+      expect(Merchant.most_revenue).to eq([@merchant_2, @merchant_3, @merchant_1])
+    end
+
+    it 'most_items' do
+      expect(Merchant.most_items(1)).to eq([@merchant_1])
+      expect(Merchant.most_items).to eq([@merchant_1, @merchant_3, @merchant_2])
+    end
+  end
 end
