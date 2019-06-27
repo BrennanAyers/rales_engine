e2142a11c1bbafbba2b00384e0a55261fdc4725f
Brennan Ayers <brennan.f.ayers@gmail.com>
2019-06-27 14:33:10 -0600

    Add Transactions to Merchants Most Revenue/Most Items specs

--- spec/requests/api/v1/merchants/most_items_request_spec.rb
+++ spec/requests/api/v1/merchants/most_items_request_spec.rb
@@ -12,10 +12,13 @@ describe 'Merchants Most Items API' do
     @item_3 = create(:item, unit_price: 200, merchant: @merchant_3)
     @invoice_1 = create(:invoice, merchant: @merchant_1, customer: @customer_1)
     @invoice_item_1 = create(:invoice_item, quantity: 3, unit_price: 100, invoice: @invoice_1, item: @item_1)
+    @transaction_1 = create(:transaction, invoice: @invoice_1)
     @invoice_2 = create(:invoice, merchant: @merchant_2, customer: @customer_2)
     @invoice_item_2 = create(:invoice_item, quantity: 2, unit_price: 1000, invoice: @invoice_2, item: @item_2)
+    @transaction_2 = create(:transaction, invoice: @invoice_2)
     @invoice_3 = create(:invoice, merchant: @merchant_3, customer: @customer_2)
     @invoice_item_3 = create(:invoice_item, quantity: 10, unit_price: 200, invoice: @invoice_3, item: @item_3)
+    @transaction_3 = create(:transaction, invoice: @invoice_3)
   end
 
   it 'sends a list of merchants by most items sold' do
