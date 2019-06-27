996847241d50ea6d1fc4930fe9f49f10ec282d36
Brennan Ayers <brennan.f.ayers@gmail.com>
2019-06-27 11:45:23 -0600

    Clean up Merchant::most_revenue method

--- app/models/merchant.rb
+++ app/models/merchant.rb
@@ -3,10 +3,10 @@ class Merchant < ApplicationRecord
   has_many :invoices
 
   def self.most_revenue(amount)
-    Merchant.joins(invoices: :invoice_items)
-            .select("merchants.*, SUM (invoice_items.quantity * invoice_items.unit_price) AS revenue")
-            .group("merchants.id")
-            .order("revenue DESC")
-            .limit(amount)
+    joins(invoices: :invoice_items)
+    .select("merchants.*, SUM (invoice_items.quantity * invoice_items.unit_price) AS revenue")
+    .group("merchants.id")
+    .order("revenue DESC")
+    .limit(amount)
   end
 end
