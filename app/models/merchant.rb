4a9cdfe1ea375beab29c34709004308e5429d282
Brennan Ayers <brennan.f.ayers@gmail.com>
2019-06-27 11:29:57 -0600

    Add ::most_revenue method to the Merchant model

--- app/models/merchant.rb
+++ app/models/merchant.rb
@@ -1,4 +1,12 @@
 class Merchant < ApplicationRecord
   has_many :items
   has_many :invoices
+
+  def self.most_revenue(amount)
+    Merchant.joins(invoices: :invoice_items)
+            .select("merchants.*, SUM (invoice_items.quantity * invoice_items.unit_price) AS revenue")
+            .group("merchants.id")
+            .order("revenue DESC")
+            .limit(amount)
+  end
 end
