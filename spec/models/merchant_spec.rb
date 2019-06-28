cc71f3a5fafe3c86aef28830e9c9fc59fa9fd304
Brennan Ayers <brennan.f.ayers@gmail.com>
2019-06-28 10:55:46 -0600

    Add Merchants::revenue model spec

--- spec/models/merchant_spec.rb
+++ spec/models/merchant_spec.rb
@@ -16,13 +16,13 @@ RSpec.describe Merchant, type: :model do
       @item_1 = create(:item, unit_price: 100, merchant: @merchant_1)
       @item_2 = create(:item, unit_price: 1000, merchant: @merchant_2)
       @item_3 = create(:item, unit_price: 200, merchant: @merchant_3)
-      @invoice_1 = create(:invoice, merchant: @merchant_1, customer: @customer_1)
+      @invoice_1 = create(:invoice, merchant: @merchant_1, customer: @customer_1, updated_at: "2012-04-01 09:53:09 UTC")
       @invoice_item_1 = create(:invoice_item, quantity: 30, unit_price: 100, invoice: @invoice_1, item: @item_1)
       @transaction_1 = create(:transaction, invoice: @invoice_1)
-      @invoice_2 = create(:invoice, merchant: @merchant_2, customer: @customer_2)
+      @invoice_2 = create(:invoice, merchant: @merchant_2, customer: @customer_2, updated_at: "2012-04-01 09:54:09 UTC")
       @invoice_item_2 = create(:invoice_item, quantity: 20, unit_price: 1000, invoice: @invoice_2, item: @item_2)
       @transaction_2 = create(:transaction, invoice: @invoice_2)
-      @invoice_3 = create(:invoice, merchant: @merchant_3, customer: @customer_2)
+      @invoice_3 = create(:invoice, merchant: @merchant_3, customer: @customer_2, updated_at: "2012-04-02 09:54:09 UTC")
       @invoice_item_3 = create(:invoice_item, quantity: 21, unit_price: 200, invoice: @invoice_3, item: @item_3)
       @transaction_3 = create(:transaction, invoice: @invoice_3)
     end
@@ -35,6 +35,11 @@ RSpec.describe Merchant, type: :model do
     it 'most_items' do
       expect(Merchant.most_items(1)).to eq([@merchant_1])
       expect(Merchant.most_items).to eq([@merchant_1, @merchant_3, @merchant_2])
+    end
+
+    it 'revenue' do
+      expect(Merchant.revenue("2012-04-01")).to eq(23000)
+      expect(Merchant.revenue("2012-04-02")).to eq(4200)
     end
   end
 end
