542812d841122dd3fde58c036af55072e9d0fd7f
Brennan Ayers <brennan.f.ayers@gmail.com>
2019-06-29 15:34:27 -0600

    Add #best_day spec for Item model

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -37,4 +37,34 @@ RSpec.describe Item, type: :model do
       expect(Item.most_items).to eq([@item_1, @item_3, @item_2])
     end
   end
+
+  describe 'instance methods' do
+    before :each do
+      @merchant_1 = create(:merchant)
+      @merchant_2 = create(:merchant)
+      @merchant_3 = create(:merchant)
+      @customer_1 = create(:customer)
+      @customer_2 = create(:customer)
+      @item = create(:item, unit_price: 100, merchant: @merchant_1)
+      @invoice_1 = create(:invoice, merchant: @merchant_1, customer: @customer_1, updated_at: "2012-04-01 05:54:09 UTC")
+      @invoice_item_1 = create(:invoice_item, quantity: 10, unit_price: 100, invoice: @invoice_1, item: @item)
+      @transaction_1 = create(:transaction, invoice: @invoice_1)
+      @invoice_2 = create(:invoice, merchant: @merchant_2, customer: @customer_2, updated_at: "2012-04-02 05:54:09 UTC")
+      @invoice_item_2 = create(:invoice_item, quantity: 4, unit_price: 1000, invoice: @invoice_2, item: @item)
+      @transaction_2 = create(:transaction, invoice: @invoice_2)
+      @invoice_3 = create(:invoice, merchant: @merchant_3, customer: @customer_2, updated_at: "2012-04-03 05:54:09 UTC")
+      @invoice_item_3 = create(:invoice_item, quantity: 2, unit_price: 200, invoice: @invoice_3, item: @item)
+      @transaction_3 = create(:transaction, invoice: @invoice_3)
+    end
+
+    it 'best_day' do
+      expect(@item.best_day.updated_at).to eq(@invoice_2.updated_at)
+
+      @invoice_4 = create(:invoice, merchant: @merchant_2, customer: @customer_2, updated_at: Date.yesterday)
+      @invoice_item_2 = create(:invoice_item, quantity: 4, unit_price: 1000, invoice: @invoice_4, item: @item)
+      @transaction_2 = create(:transaction, invoice: @invoice_4)
+
+      expect(@item.best_day.updated_at).to eq(@invoice_4.updated_at)
+    end
+  end
 end
