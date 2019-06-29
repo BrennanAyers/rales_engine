c9231a8381822b6d2388c4cd57c8784b4914601d
Brennan Ayers <brennan.f.ayers@gmail.com>
2019-06-28 20:27:23 -0600

    Add new RevenueSerializer for Merchant::RevenueController#show

--- app/serializers/revenue_serializer.rb
+++ app/serializers/revenue_serializer.rb
@@ -1,10 +1,10 @@
 class RevenueSerializer
   include FastJsonapi::ObjectSerializer
-  attribute :total_revenue do |object|
-    if object.total_revenue.nil?
+  attribute :revenue do |object|
+    if object.revenue.nil?
       "0.00"
     else
-      "#{'%.2f' % object.total_revenue.fdiv(100)}"
+      "#{'%.2f' % object.revenue.fdiv(100)}"
     end
   end
 end
