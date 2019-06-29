27761fba90309f7ea99ebb7fd9d5904eaef8e95a
Brennan Ayers <brennan.f.ayers@gmail.com>
2019-06-29 13:36:59 -0600

    Add Items/best_day route to #show action

--- config/routes.rb
+++ config/routes.rb
@@ -16,6 +16,7 @@ Rails.application.routes.draw do
       namespace :items do
         get 'most_revenue', to: 'most_revenue#index'
         get 'most_items', to: 'most_items#index'
+        get 'best_day', to: 'best_day#show'
       end
       resources :merchants, only: [:index, :show]
       resources :customers, only: [:index, :show]
