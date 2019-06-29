af028207de37840a949052b631c80646017f7e7e
Brennan Ayers <brennan.f.ayers@gmail.com>
2019-06-28 20:27:45 -0600

    Add instance methods spec section for Merchants#revenue

--- spec/models/merchant_spec.rb
+++ spec/models/merchant_spec.rb
@@ -51,4 +51,37 @@ RSpec.describe Merchant, type: :model do
       expect(Merchant.revenue["total_revenue"]).to eq(1500)
     end
   end
+
+  describe 'instance methods' do
+    before :each do
+      @merchant_1 = create(:merchant)
+      @customer_1 = create(:customer)
+      @customer_2 = create(:customer)
+      @item_1 = create(:item, unit_price: 100, merchant: @merchant_1)
+      @item_2 = create(:item, unit_price: 1000, merchant: @merchant_1)
+      @item_3 = create(:item, unit_price: 200, merchant: @merchant_1)
+      @invoice_1 = create(:invoice, merchant: @merchant_1, customer: @customer_1, updated_at: "2012-04-01 09:53:09 UTC")
+      @invoice_item_1 = create(:invoice_item, quantity: 30, unit_price: 100, invoice: @invoice_1, item: @item_1)
+      @transaction_1 = create(:transaction, invoice: @invoice_1)
+      @invoice_2 = create(:invoice, merchant: @merchant_1, customer: @customer_2, updated_at: "2012-04-01 09:54:09 UTC")
+      @invoice_item_2 = create(:invoice_item, quantity: 20, unit_price: 1000, invoice: @invoice_2, item: @item_2)
+      @transaction_2 = create(:transaction, invoice: @invoice_2)
+      @invoice_3 = create(:invoice, merchant: @merchant_1, customer: @customer_2, updated_at: "2012-04-02 09:54:09 UTC")
+      @invoice_item_3 = create(:invoice_item, quantity: 21, unit_price: 200, invoice: @invoice_3, item: @item_3)
+      @transaction_3 = create(:transaction, invoice: @invoice_3)
+    end
+
+    it 'revenue' do
+      expect(@merchant_1.revenue("2012-04-01")["revenue"]).to eq(23000)
+      expect(@merchant_1.revenue("2012-04-02")["revenue"]).to eq(4200)
+
+      customer_4 = create(:customer)
+      item_4 = create(:item, unit_price: 100, merchant: @merchant_1)
+      invoice_4 = create(:invoice, merchant: @merchant_1, customer: customer_4, updated_at: Date.today)
+      invoice_item_4 = create(:invoice_item, quantity: 15, unit_price: 100, invoice: invoice_4, item: item_4)
+      transaction_4 = create(:transaction, invoice: invoice_4)
+
+      expect(@merchant_1.revenue["revenue"]).to eq(28700)
+    end
+  end
 end
