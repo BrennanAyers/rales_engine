5d2e7c891d2d41cd001b6104565b883f900d6dd2
Brennan Ayers <brennan.f.ayers@gmail.com>
2019-06-29 17:36:56 -0600

    Add Items::FindController with #show

---
+++ app/controllers/api/v1/items/find_controller.rb
@@ -0,0 +1,12 @@
+class Api::V1::Items::FindController < ApplicationController
+  def show
+    finders = [:id, :name, :description, :merchant_id, :unit_price, :created_at, :updated_at]
+    first_finder = finders.map {|key| key if params[key]}.compact.first
+    if first_finder == :unit_price
+      merchant = Item.find_by(first_finder => (params[first_finder].to_f * 100).round)
+    else
+      merchant = Item.find_by(first_finder => params[first_finder])
+    end
+    render json: ItemSerializer.new(merchant)
+  end
+end
