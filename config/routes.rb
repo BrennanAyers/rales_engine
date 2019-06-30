1e51b52f5c042a780e796e58ad373c30d7362c23
Brennan Ayers <brennan.f.ayers@gmail.com>
2019-06-30 00:13:47 -0600

    Add Transactions/find_all route to #index

--- config/routes.rb
+++ config/routes.rb
@@ -40,6 +40,7 @@ Rails.application.routes.draw do
 
       namespace :transactions do
         get '/find', to: 'find#show'
+        get '/find_all', to: 'find#index'
       end
       resources :merchants, only: [:index, :show]
       resources :customers, only: [:index, :show]
