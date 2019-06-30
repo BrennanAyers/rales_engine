aef0d6f2586929b1bb499effc505a6cd501e1daf
Brennan Ayers <brennan.f.ayers@gmail.com>
2019-06-29 18:25:40 -0600

    Add Transactions::FindController with #show

---
+++ app/controllers/api/v1/transactions/find_controller.rb
@@ -0,0 +1,8 @@
+class Api::V1::Transactions::FindController < ApplicationController
+  def show
+    finders = [:id, :invoice_id, :credit_card_number, :result, :created_at, :updated_at]
+    first_finder = finders.map {|key| key if params[key]}.compact.first
+    merchant = Transaction.find_by(first_finder => params[first_finder])
+    render json: TransactionSerializer.new(merchant)
+  end
+end
