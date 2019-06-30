40fff08277edb1ddf71fd401eb1d5d08d7863ed5
Brennan Ayers <brennan.f.ayers@gmail.com>
2019-06-29 19:59:06 -0600

    Add Invoices/find_all route to #index

--- config/routes.rb
+++ config/routes.rb
@@ -30,6 +30,7 @@ Rails.application.routes.draw do
 
       namespace :invoices do
         get '/find', to: 'find#show'
+        get '/find_all', to: 'find#index'
       end
 
       namespace :invoice_items do
