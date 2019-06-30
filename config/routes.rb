651e74230c672af288a8ec1be334f60b75458792
Brennan Ayers <brennan.f.ayers@gmail.com>
2019-06-30 17:45:08 -0600

    Add Customers/transactions route to #index action

--- config/routes.rb
+++ config/routes.rb
@@ -20,8 +20,9 @@ Rails.application.routes.draw do
 
         get '/find', to: 'find#show'
         get '/find_all', to: 'find#index'
-        
+
         get '/:id/invoices', to: 'invoices#index'
+        get '/:id/transactions', to: 'transactions#index'
       end
 
       namespace :items do
