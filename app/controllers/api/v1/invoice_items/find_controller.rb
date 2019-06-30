441676adf13f274da47541b35dd08085602ef724
Brennan Ayers <brennan.f.ayers@gmail.com>
2019-06-30 00:02:50 -0600

    Add #index method for InvoiceItems::FindController

--- app/controllers/api/v1/invoice_items/find_controller.rb
+++ app/controllers/api/v1/invoice_items/find_controller.rb
@@ -9,4 +9,15 @@ class Api::V1::InvoiceItems::FindController < ApplicationController
     end
     render json: InvoiceItemSerializer.new(invoice_item)
   end
+
+  def index
+    finders = [:id, :item_id, :invoice_id, :quantity, :unit_price, :created_at, :updated_at]
+    first_finder = finders.map {|key| key if params[key]}.compact.first
+    if first_finder == :unit_price
+      invoice_items = InvoiceItem.where(first_finder => (params[first_finder].to_f * 100).round)
+    else
+      invoice_items = InvoiceItem.where(first_finder => params[first_finder])
+    end
+    render json: InvoiceItemSerializer.new(invoice_items)
+  end
 end
