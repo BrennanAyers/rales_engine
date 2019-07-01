d752605b548cb9dd8134da8735960defac185aca
Brennan Ayers <brennan.f.ayers@gmail.com>
2019-06-30 19:10:42 -0600

    Add InvoiceItems/invoice route to #index action

--- config/routes.rb
+++ config/routes.rb
@@ -51,6 +51,8 @@ Rails.application.routes.draw do
       namespace :invoice_items do
         get '/find', to: 'find#show'
         get '/find_all', to: 'find#index'
+
+        get '/:id/invoice', to: 'invoice#index'
       end
 
       namespace :transactions do
